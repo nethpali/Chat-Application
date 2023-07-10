42eb6d8e41d65a6af3280982ec0ddddc4868faf4
Nethpali_Jayaweera <nethpalijayaweera@gmail.com>
2023-07-10 08:41:41 +0530

    Client Controller

--- src/main/java/ClientFormController.java
+++ src/main/java/ClientFormController.java
@@ -1,14 +1,33 @@
-import com.jfoenix.controls.JFXTextArea;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ClientFormController {
+    public VBox VboxSendMessage;
     public Label lblUserName;
-    public JFXTextArea txtAreaMessage;
-    public TextField txtFeildMessage;
+    public TextField txtFieldMessage;
     public Button btnSend;
+
+
+    ServerSocket serverSocket;
+    Socket socket;
+    DataOutputStream dataOutputStream;
+    DataInputStream dataInputStream;
+    String  message="";
+
+    public void setLable(String text) {
+
+        lblUserName.setText(text);
+    }
+
 
     public void btnSendOnAction(ActionEvent actionEvent) {
     }
