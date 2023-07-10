3957c0f0c6219bf11569995ee93339efd6519d5a
Nethpali_Jayaweera <nethpalijayaweera@gmail.com>
2023-07-10 08:43:21 +0530

    Login Controller

--- src/main/java/LoginController.java
+++ src/main/java/LoginController.java
@@ -1,11 +1,7 @@
-import Model.UserModel;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
@@ -14,41 +10,23 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
-import java.sql.SQLException;
-import java.util.List;
 import java.util.ResourceBundle;
 
-public class LoginController implements Initializable {
+public class LoginController {
     public Button btnLogIn;
     public TextField txtUserName;
     public Button btnSignUp;
     public ComboBox cmbUserName;
     private String userId;
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        CmbLoaUserName();
-    }
-
-    private void CmbLoaUserName() {
-        try {
-            ObservableList<String> obList = FXCollections.observableArrayList();
-            List<String> codes = UserModel.getUserName();
-
-            for (String code : codes) {
-                obList.add(code);
-            }
-            cmbUserName.setItems(obList);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            new Alert(Alert.AlertType.ERROR, "SQL Error!").show();
-        }
-    }
-
+    String name;
+    ClientFormController clientFormController;
 
     public void btnLoginOnAction(ActionEvent actionEvent) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("View/ClientForm.fxml"));
         AnchorPane anchorPane = loader.load();
+        clientFormController=loader.getController();
+        clientFormController.setLable(txtUserName.getText());
+        txtUserName.clear();
         Scene scene = new Scene(anchorPane);
         Stage stage = new Stage();
         stage.setScene(scene);
@@ -56,13 +34,4 @@ public class LoginController implements Initializable {
         stage.show();
     }
 
-    public void btnSignUpOnAction(ActionEvent actionEvent) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("View/SignUpForm.fxml"));
-        AnchorPane anchorPane = loader.load();
-        Scene scene = new Scene(anchorPane);
-        Stage stage = new Stage();
-        stage.setScene(scene);
-        stage.setTitle("Client Sign up");
-        stage.show();
-    }
 }
